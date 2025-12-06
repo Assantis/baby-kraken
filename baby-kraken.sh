485e6bdadca6cc594525cfa36418bc6de8d1f7f3
Assantis <alexander@knakrick.de>
2025-12-06 10:59:27 +0000

    fix: better wording

--- baby-kraken.sh
+++ baby-kraken.sh
@@ -12,10 +12,10 @@ NC=$'\033[0m'
 
 echo "${BLUE}Checking git status 🔍 ... ${NC}"
 
-# if ! git diff-index --quiet HEAD --; then
-#   echo "${RED}You have uncommitted changes. Please commit or stash them before releasing.${NC}"
-#   exit 1
-# fi
+if ! git diff-index --quiet HEAD --; then
+  echo "${RED}You have uncommitted changes. Please commit or stash them before releasing.${NC}"
+  exit 1
+fi
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "${BLUE}Current branch: ${CYAN}${current_branch}${NC}"
@@ -23,9 +23,9 @@ echo "${BLUE}Current branch: ${CYAN}${current_branch}${NC}"
 is_hotfix=false
 
 if [[ "$current_branch" == "develop" ]]; then
-  echo "${GREEN}On develop normal release${NC}"
+  echo "${GREEN}Develop branch detected => normal release${NC}"
 elif [[ "$current_branch" =~ ^hotfix/ ]]; then
-  echo "${YELLOW}Hotfix detected${NC}"
+  echo "${YELLOW}Hotfix branch detected$ => hotfix release${NC}"
   is_hotfix=true
 else
   echo "${RED}You must be on 'develop' or 'hotfix/*' to create a release.${NC}"
