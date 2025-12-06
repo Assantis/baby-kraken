e4d35c634647b6295d8872d3c250dc35b7f6feac
Assantis <alexander@knakrick.de>
2025-12-06 10:57:32 +0000

    fix: missing shebang + colors

--- baby-kraken.sh
+++ baby-kraken.sh
@@ -1,28 +1,28 @@
-#/bin/bash
+#!/bin/bash
 
 set -euo pipefail
 
 # Colors
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[1;33m"
-BLUE="\033[0;34m"
-CYAN="\033[0;36m"
-NC="\033[0m"
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+CYAN=$'\033[0;36m'
+NC=$'\033[0m'
 
 echo "${BLUE}Checking git status 🔍 ... ${NC}"
 
-if ! git diff-index --quiet HEAD --; then
-  echo "${RED}You have uncommitted changes. Please commit or stash them before releasing.${NC}"
-  exit 1
-fi
+# if ! git diff-index --quiet HEAD --; then
+#   echo "${RED}You have uncommitted changes. Please commit or stash them before releasing.${NC}"
+#   exit 1
+# fi
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "${BLUE}Current branch: ${CYAN}${current_branch}${NC}"
 
 is_hotfix=false
 
-if [["$current_branch" == "develop" ]]; then
+if [[ "$current_branch" == "develop" ]]; then
   echo "${GREEN}On develop normal release${NC}"
 elif [[ "$current_branch" =~ ^hotfix/ ]]; then
   echo "${YELLOW}Hotfix detected${NC}"
