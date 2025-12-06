2cd51daf02c6325cdb472429744e36dd8f68f56b
Assantis <alexander@knakrick.de>
2025-12-06 10:46:29 +0000

    feat: add the first checks

---
+++ baby-kraken.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+set -euo pipefail
+
+# Colors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+CYAN="\033[0;36m"
+NC="\033[0m"
+
+echo "${BLUE}Checking git status 🔍 ... ${NC}"
+
+if ! git diff-index --quiet HEAD --; then
+  echo "${RED}You have uncommitted changes. Please commit or stash them before releasing.${NC}"
+  exit 1
+fi
+
+
+is_hotfix=false
+
+if [[ "$current_branch" == "develop" ]]; then
+  echo -e "${GREEN}✔ On develop – normal release${NC}"
+elif [[ "$current_branch" =~ ^hotfix/ ]]; then
+  echo -e "${YELLOW}✔ Hotfix detected${NC}"
+  is_hotfix=true
+else
+  echo -e "${RED}You must be on 'develop' or 'hotfix/*' to create a release.${NC}"
+  exit 1
+fi
